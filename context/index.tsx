4e8212db455af040d3797f512c0a2bf1678b0be1
Jose Banega <banegajose26@gmail.com>
2021-05-30 22:14:18 -0300

    fix: issue with localstorage in deploy

--- context/index.tsx
+++ context/index.tsx
@@ -1,10 +1,8 @@
-import  { createContext, useReducer, Dispatch, useEffect } from "react";
+import { createContext, useReducer, Dispatch, useEffect } from "react";
 import { getInitStateFromStorage } from "utils";
 import { setState } from "./action";
 import { postReducer, initialState, InitialState } from "./reducer";
 import { AppActions } from "./type";
-
-
 
 export const AppContext = createContext<{
   state: InitialState;
@@ -14,11 +12,12 @@ export const AppContext = createContext<{
 export const AppProvider: React.FC = ({ children }) => {
   const [state, dispatch] = useReducer(postReducer, initialState);
 
-  useEffect(()=>{
-    dispatch(setState(getInitStateFromStorage() as InitialState))
-  },[
-    
-  ])
+  useEffect(() => {
+    const localStorageState = getInitStateFromStorage();
+    if (localStorageState) {
+      dispatch(setState(localStorageState as InitialState));
+    }
+  }, []);
   useEffect(() => {
     localStorage.setItem("state", JSON.stringify(state));
   }, [state]);
